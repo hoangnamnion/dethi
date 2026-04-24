b1d889e3059294882345fb48ebb37edb6b79af4a
Cao Văn Nam <189972221+hoangnamnion@users.noreply.github.com>
2026-04-24 13:34:47 +0700

    Update hihihaaaa.js

--- js/hihihaaaa.js
+++ js/hihihaaaa.js
@@ -8,9 +8,26 @@ const getTelegramChatId = () => atob(_T_CHAT_);
 
 // 2. DANH SÁCH ĐỀ THI MẶC ĐỊNH (Dành cho ai không được gán đề riêng)
 const DEFAULT_EXAMS = [
-    { file: '1', ten: 'Vật lý kiến trúc 35 câu đầu' },
-    { file: '2', ten: 'Vật lý kiến trúc 35 câu tiếp' },
-    { file: '3', ten: 'Vật lý kiến trúc 70 Câu' },
+    { file: 'luatxd1', ten: 'Luật Xây Dựng 1-30 câu' },
+    { file: 'luatxd2', ten: 'Luật Xây Dựng 31-64 câu' },
+    { file: 'luatxd3', ten: 'Luật Xây Dựng 61-90 câu' },
+    { file: 'luatxd4', ten: 'Luật Xây Dựng 91-120 câu' },
+    { file: 'luatxd5', ten: 'Luật Xây Dựng 121-137 câu' },
+    { file: 'luatxd', ten: 'Full Luật Xây Dựng 137 câu' },
+    { file: 'cnxhkh1', ten: 'CNXHKH Chương I 1-20 câu' },
+    { file: 'cnxhkh2', ten: 'CNXHKH Chương II 1-30 câu' },
+    { file: 'cnxhkh3', ten: 'CNXHKH Chương II 31-65 câu' },
+    { file: 'cnxhkh4', ten: 'CNXHKH Chương III 1-30 câu' },
+    { file: 'cnxhkh5', ten: 'CNXHKH Chương III 31-50 câu' },
+    { file: 'cnxhkh6', ten: 'CNXHKH Chương IV 1-30 câu' },
+    { file: 'cnxhkh7', ten: 'CNXHKH Chương IV 31-50 câu' },
+    { file: 'cnxhkh8', ten: 'CNXHKH Chương V 1-30 câu' },
+    { file: 'cnxhkh9', ten: 'CNXHKH Chương V 31-50 câu' },
+    { file: 'cnxhkh10', ten: 'CNXHKH Chương VI 1-30 câu' },
+    { file: 'cnxhkh11', ten: 'CNXHKH Chương VI 31-60 câu' },
+    { file: 'cnxhkh12', ten: 'CNXHKH Chương VII 1-30 câu' },
+    { file: 'cnxhkh13', ten: 'CNXHKH Chương VII 31-55 câu' },
+    { file: 'cnxhkh', ten: 'Full VII Chương CNXHKH' },
 
 
     { ten: 'Trang Có Đề Thi', url: 'sbvl.html', IMG: 'anhnen/pdf.gif' }
@@ -20,7 +37,7 @@ const DEFAULT_EXAMS = [
 // Cấu trúc: "TàiKhoản": { pass: "MậtKhẩu", name: "Tên Hiển Thị", exams: [...] }
 // Nếu bỏ trống "exams", hệ thống sẽ tự nạp DEFAULT_EXAMS ở trên.
 const VALID_ACCOUNTS = {
-    "admin": { pass: "admin123", name: "Quản Trị Viên", id: ["DEV_GMF5XTPQ", "DEV_23IKO5SS", "DEV_UCAISH7Y", "DEV_DXN4ABL9", "DEV_SDG1EUU7",] },
+    "admin": { pass: "hoangnam123", name: "Quản Trị Viên", id: ["DEV_GMF5XTPQ", "DEV_23IKO5SS", "DEV_UCAISH7Y", "DEV_DXN4ABL9", "DEV_SDG1EUU7",] },
     "hung": { pass: "hung123", name: "Idol Hưng Đẹp Zai", id: "DEV_GDHUTRNB", },
     "trung": { pass: "trung123", name: "Quang Trung", id: "DEV_RTM2ST30", },
     "quyen": { pass: "quyen123", name: "Văn Quyến" },
