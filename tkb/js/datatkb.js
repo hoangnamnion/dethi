b43aaa678b22bf4023e6f7dfc8bca6a840a2b0b8
Cao Văn Nam <189972221+hoangnamnion@users.noreply.github.com>
2026-05-07 10:15:01 +0700

    Add files via upload

---
+++ tkb/js/datatkb.js
@@ -0,0 +1,10 @@
+const tkbData = [
+    { day: "Thứ 2", period: 1, subject: "Cơ học kết cấu", teacher: "Cô Lê Thị Như Trang", room: "A2.101", time: "12:45 - 15:25" },
+    { day: "Thứ 2", period: 2, subject: "Kiến trúc dân dụng và công nghiệp", teacher: "Thầy Vũ Đức Tâm", room: "A2.101", time: "15:30 - 18:10" },
+    { day: "Thứ 3", period: 1, subject: "Cơ học kết cấu", teacher: "Cô Lê Thị Như Trang", room: "A2.104", time: "12:45 - 17:15" },
+    { day: "Thứ 4", period: 1, subject: "Chủ nghĩa xã hội khoa học", teacher: "Cô Phạm Bích Hằng", room: "A2.502", time: "12:45 - 17:15" },
+    { day: "Thứ 5", period: 1, subject: "Luật xây dựng", teacher: "Thầy Nguyễn Văn Hiền", room: "A2.101", time: "12:45 - 15:25" },
+    { day: "Thứ 6", period: 1, subject: "Kiến trúc dân dụng và công nghiệp", teacher: "Thầy Vũ Đức Tâm", room: "A1.201", time: "12:45 - 17:15" },
+    { day: "Thứ 7", period: 1, subject: "Nghỉ Học", teacher: "", room: "", time: "" },
+    { day: "Chủ Nhật", period: 1, subject: "Nghỉ Học", teacher: "", room: "", time: "" }
+];
