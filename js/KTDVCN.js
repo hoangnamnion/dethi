e696ef6e0a212b46822091bbfc49cdc4c1a4c04f
Cao Văn Nam <189972221+hoangnamnion@users.noreply.github.com>
2026-06-29 01:36:07 +0700

    Update KTDVCN.js

--- js/KTDVCN.js
+++ js/KTDVCN.js
@@ -2,7 +2,7 @@ const DEFAULT_EXAMS = [
     { ten: 'Trang Chủ', url: 'index.html', IMG: 'img/muiten.jpg' },
 
     { file: 'ddvcnt', ten: '100 Câu Trắc Nghiệm KTDVCN' },
-    { ten: 'Trang Tự Luận ', url: 'ktdd.html', IMG: 'img/muiten.jpg' },
+    { ten: 'Trang Tự Luận ', url: 'ktdd.html' },
 
 
 
